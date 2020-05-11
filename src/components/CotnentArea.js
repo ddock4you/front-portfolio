d70cfbd5e1f1666a8a341001cf9dcd7f05708c17
ddock4you <ddock4you@gmail.com>
2020-05-11 20:13:01 +0900

    - 콘텐츠 영역 추가 중

--- src/components/CotnentArea.js
+++ src/components/CotnentArea.js
@@ -1,7 +1,12 @@
 import React from "react";
+import FrontWorks from "./FrontWorks";
 
 const CotnentArea = () => {
-    return <div className="content">컨텐츠 영역</div>;
+    return (
+        <div className="content">
+            <FrontWorks />
+        </div>
+    );
 };
 
 export default CotnentArea;
